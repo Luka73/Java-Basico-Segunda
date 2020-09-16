0674f8e48f9faf5934c00b29635bfd7f2d072234
Luka <luanafernandes73@gmail.com>
2020-09-15 22:25:47 -0400

    Aula01

---
+++ src/OlaMundo.java
@@ -0,0 +1,9 @@
+public class OlaMundo { // classe
+
+    public static void main(String[] args) { // função principal de execução do código
+        System.out.println("Olá, Mundo1!"); //ln --> pula linha
+        System.out.print("Olá, Mundo2!"); // sem ln --> não pula linha
+        System.out.print("Olá, Mundo3!");
+    } //indentação
+
+} // escopo
---
+++ src/Variaveis.java
@@ -0,0 +1,27 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        System.out.println("Pedro"); //String
+        System.out.println("28"); //String --> texto
+        System.out.println(28); // int --> integer (números inteiros)
+        System.out.println(28.5); // double --> números com casas decimais
+        //as casas são marcadas com ponto e não com vírgula
+
+        System.out.println(true); // boolean --> lógico
+        System.out.println(false);
+
+        System.out.println("Pedro " + "28"); // + --> concatenação = Juntar!!
+        System.out.println(28 + 10); // 38
+        System.out.println("28" + "10"); // 2810
+        System.out.println("28" + 10); // 2810
+
+        String nome = "Pedro";
+        int idade = 28;
+        double altura = 1.78;
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(nome + " tem " + idade + "anos");
+
+    }
+}
